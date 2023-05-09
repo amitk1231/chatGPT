9e87a9238d5c048610e43ae75330ea804fd83554
Amit Kumar <amitk@maqsoftware.com>
2023-05-09 14:11:48 +0530

    AI Bot 1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'chatGPT';
+  links = [
+    { path: '/home', label: 'Home', active: 'button-active' },
+    { path: '/chat', label: 'Chat', active: 'button-active' },
+    { path: '/signin', label: 'Signin', active: 'button-active' },
+  ];
 }
