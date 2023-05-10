185fb4107e8fae79aa508ccfa6eeb4c17107d0a3
Amit Kumar <amitk@maqsoftware.com>
2023-05-10 17:22:12 +0530

    10th May 2nd commit

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+export class Message{
+  constructor(public author: string, public content: string){}
+}
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,25 @@ import { Injectable } from '@angular/core';
 export class ChatService {
 
   constructor() { }
+  conversation = new Subject<Message[]>();
+  messageMap: any = {
+    "Hi": "Hello",
+    "Who are you?": "I'm chat bot",
+    "default": "I couldn't get it. Can you plz repeat?"
+  }
+  getBotAnswer(msg: any){
+    const userMessage = new Message('user', msg);
+    this.conversation.next([userMessage]);
+    const botMsg = new Message('bot', this.getBotMessage(msg));
+    setTimeout(() => {
+      this.conversation.next([botMsg]);
+    }, 1000)
+  }
+
+  getBotMessage(question: string) {
+      let answer = this.messageMap[question];
+      return answer || this.messageMap['default'];
+  }
+
+  
 }
