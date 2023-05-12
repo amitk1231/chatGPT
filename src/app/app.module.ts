74d1c1fec5d10e0905b28154206bb67f6603c22c
Amit Kumar <amitk@maqsoftware.com>
2023-05-12 14:59:04 +0530

    12th May Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent {
+  constructor(private router: Router) {}
 
+  title: string = 'Sign-up Form';
+
+  signupForm1 = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z ]*'),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+  });
+
+  signupUser() {
+    console.log(this.signupForm1.value);
+    alert('Successfully signed up');
+    this.router.navigate(['/home']);
+  }
+  get name() {
+    return this.signupForm1.get('name');
+  }
+  get email() {
+    return this.signupForm1.get('email');
+  }
+  get password() {
+    return this.signupForm1.get('password');
+  }
 }
